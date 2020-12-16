4a0ce49b7f80d876a486bfca3dcdae65bdca6709
tjddus <tjddus424@naver.com>
2020-12-16 17:12:55 +0900

    2440 별찍기3

---
+++ BOJ/02000/2440_별찍기3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N;
+    cin >> N;
+    for (int i = N; i >= 1; i--)
+    {
+        for (int j = i; j >= 1; j--)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
