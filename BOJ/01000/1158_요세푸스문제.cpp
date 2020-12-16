bc6e5aa14740285be7dd71134fea9d2dda116fa1
tjddus <tjddus424@naver.com>
2020-12-16 14:09:54 +0900

    1158 요세푸스 문제

---
+++ BOJ/01000/1158_요세푸스문제.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    int N, K;
+    queue<int> q;
+    cin >> N >> K;
+    for (int i = 1; i <= N; i++)
+        q.push(i);
+
+    cout << "<";
+    while (q.size() != 1)
+    {
+        for (int i = 1; i < K; i++)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+        cout << q.front() << ", ";
+        q.pop();
+    }
+    cout << q.front() << ">";
+    return 0;
+}
