072ada4fe6b50e26ce0688cff34c178adeb704ee
tjddus <tjddus424@naver.com>
2020-12-19 17:13:56 +0900

    2609 최대공약수와 최소공배수

---
+++ BOJ/02000/2609_최대공약수와최소공배수.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int GCD(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return GCD(b, a % b);
+}
+
+int main()
+{
+    int a, b, gcd;
+    cin >> a >> b;
+    if(a > b) gcd = GCD(a, b);
+    else gcd = GCD(b, a);
+    
+    cout << gcd << "\n";
+    cout << (a * b) / gcd << "\n";
+    return 0;
+}
+
+// #1 런타임에러
+/*
+    반례 : 18 29
+    처음 짠 코드의 경우 
+    29 / 18 = 1 ... 11
+    18 / 11 = 1 ... 7
+    18 / 7  = 2 ... 4
+    18 / 4  = 4 ... 2
+    18 / 2  = 9 ... 0
+    18 / 2 && 29 / 2 (x) 이후에 나눠야할 수가 0이 되고 0으로 나누니 당연히 런타임 에러
+
+    6 10 
+    10 / 6 = 1 ... 4
+    6 / 4  = 1 ... 2
+    6 / 2  = 1 ... 0
+    6 / 2 = 0 && 10 / 2 = 0 이니 이런 문제들은 잘 맞추었다.
+*/
+
+
