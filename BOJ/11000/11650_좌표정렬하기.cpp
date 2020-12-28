ce33471c5b73a38cbe010b83c70f7e2e30f2b446
tjddus <tjddus424@naver.com>
2020-12-29 05:07:19 +0900

    11650 좌표정렬하기

---
+++ BOJ/11000/11650_좌표정렬하기.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+pair<int, int> a[100001];
+
+bool compare(pair<int, int> a, pair<int, int> b)
+{
+    if (a.first == b.first)
+        return a.second < b.second;
+    else
+        return a.first < b.first;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i].first >> a[i].second;
+
+    sort(a, a + n, compare);
+    for (int i = 0; i < n; i++)
+        cout << a[i].first << " " << a[i].second << "\n";
+
+    return 0;
+}
