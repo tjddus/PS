18e7467a7b5153dd6f620d593a51b083b232852d
tjddus <tjddus424@naver.com>
2021-02-05 00:58:36 +0900

    10972 다음 순열

--- BOJ/10000/10972_다음순열.cpp
+++ BOJ/10000/10972_다음순열.cpp
@@ -5,6 +5,39 @@ using namespace std;
 #define MAX_N 10005
 
 int n, a[MAX_N], pos;
+bool flag = true;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        if(i > 1 && a[i-1] < a[i]) flag = false;
+    }
+
+    if(flag){
+        cout << -1 << "\n";
+        return 0;
+    }
+
+    next_permutation(a, a + n);
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+
+    return 0;
+}
+
+/* 다음 순열
+
+1. 1 2 / 4 3 > 내림차순이 끝나는 부분의 pos 값을 구함.
+2. (1) 2 < 3  (2) 2 < 4 인덱스 끝에서부터 비교
+3. 1 3 / 4 2 > pos + 1 부터 끝까지 오름차순 정렬
+
 
 int main()
 {
@@ -52,3 +85,4 @@ int main()
     }
     return 0;
 }
+*/
