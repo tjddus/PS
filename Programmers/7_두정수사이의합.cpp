39ec8305fac90e3cf91f0ec3b3794fe17f43ee9e
tjddus <tjddus424@naver.com>
2021-03-25 12:12:02 +0900

    7 두 정수사이의 합

---
+++ Programmers/7_두정수사이의합.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int a, int b)
+{
+    if (a > b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+
+    }
+
+    long long answer = 0;
+
+    for (int i = a; i <= b; i++)
+    {
+        answer += i;
+    }
+
+    return answer;
+}
